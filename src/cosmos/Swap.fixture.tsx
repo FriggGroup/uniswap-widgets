0b6a38af2e5c019455b75390f62a7280e9b20874
severinwullschleger <severin.wullschleger@bluewin.ch>
2022-07-14 16:20:55 +0200

    adding option to Swap fixture

--- src/cosmos/Swap.fixture.tsx
+++ src/cosmos/Swap.fixture.tsx
@@ -12,6 +12,7 @@ import {
 import { useEffect } from 'react'
 import { useValue } from 'react-cosmos/fixture'
 
+import { MarketType } from '../components/Swap'
 import { DAI, USDC_MAINNET } from '../constants/tokens'
 import useJsonRpcEndpoint from './useJsonRpcEndpoint'
 import useOption from './useOption'
@@ -31,7 +32,12 @@ function Fixture() {
   const currencies: Record<string, string> = {
     Native: 'NATIVE',
     DAI: DAI.address,
-    USDC: USDC_MAINNET.address,
+    USDC_Mainnet: USDC_MAINNET.address,
+    USDC_Goerli: '0x07865c6E87B9F70255377e024ace6630C1Eaa37F',
+    ATT: '0xFAf5D3E0E1511582E0C4418dE9f609C333F444f8',
+    BTT: '0x85d36Ab8F67533806eFA80982d8A0A6BF8188a98',
+    CTT: '0x615a28d4367322756400593171CeebA69773303b',
+    DTT: '0x0f710556B75091Fb7D54595AE87fBE5d133a197e',
   }
   const defaultInputToken = useOption('defaultInputToken', { options: currencies, defaultValue: 'Native' })
   const [defaultInputAmount] = useValue('defaultInputAmount', { defaultValue: 1 })
@@ -50,12 +56,79 @@ function Fixture() {
   const jsonRpcEndpoint = useJsonRpcEndpoint()
   const connector = useProvider()
 
+  const friggTokens = [
+    {
+      name: 'ATT',
+      address: '0xFAf5D3E0E1511582E0C4418dE9f609C333F444f8',
+      symbol: 'ATT',
+      decimals: 18,
+      chainId: 5,
+      logoURI:
+        'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x6B175474E89094C44Da98b954EedeAC495271d0F/logo.png',
+    },
+    {
+      name: 'BTT',
+      address: '0x85d36Ab8F67533806eFA80982d8A0A6BF8188a98',
+      symbol: 'BTT',
+      decimals: 18,
+      chainId: 5,
+      logoURI:
+        'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x6B175474E89094C44Da98b954EedeAC495271d0F/logo.png',
+    },
+    {
+      name: 'CTT',
+      address: '0x615a28d4367322756400593171CeebA69773303b',
+      symbol: 'CTT',
+      decimals: 18,
+      chainId: 5,
+      logoURI:
+        'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x6B175474E89094C44Da98b954EedeAC495271d0F/logo.png',
+    },
+    {
+      name: 'DTT',
+      address: '0x0f710556B75091Fb7D54595AE87fBE5d133a197e',
+      symbol: 'DTT',
+      decimals: 18,
+      chainId: 1,
+      logoURI:
+        'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x6B175474E89094C44Da98b954EedeAC495271d0F/logo.png',
+    },
+    // we can keep every token from every network in the list. the list is filtered by chainId during runtime
+    {
+      name: 'USD Coin',
+      address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+      symbol: 'USDC',
+      decimals: 6,
+      chainId: 1,
+      logoURI:
+        'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png',
+    },
+    {
+      name: 'USD Coin',
+      address: '0x07865c6E87B9F70255377e024ace6630C1Eaa37F',
+      symbol: 'USDC',
+      decimals: 6,
+      chainId: 5,
+      logoURI:
+        'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png',
+    },
+  ]
+
   const tokenLists: Record<string, TokenInfo[]> = {
-    Default: tokens,
+    Default: friggTokens,
     'Mainnet only': tokens.filter((token) => token.chainId === SupportedChainId.MAINNET),
+    'Frigg tokens': friggTokens,
   }
   const tokenList = useOption('tokenList', { options: tokenLists, defaultValue: 'Default', nullable: false })
   console.log(tokenList)
+
+  const marketTypes: Record<string, MarketType> = {
+    Default: 'buy',
+    buy: 'buy',
+    swap: 'swap',
+    sell: 'sell',
+  }
+  const marketType = useOption('marketType', { options: marketTypes, defaultValue: 'Default', nullable: false })
 
   return (
     <SwapWidget
@@ -72,6 +145,7 @@ function Fixture() {
       tokenList={tokenList}
       width={width}
       onConnectWallet={() => console.log('onConnectWallet')} // this handler is included as a test of functionality, but only logs
+      marketType={marketType}
     />
   )
 }
